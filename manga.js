c9a3e476cc3c665dc443132c542838c66eed2d5a
sam2much96 <sam2much96@gmail.com>
2025-02-22 23:03:49 +0100

    ads implementation 2

--- manga.js
+++ manga.js
@@ -552,16 +552,18 @@ function insertAds() {
     console.log("inserting ads");
     setAdsVisibility(true);
     adsContainer.innerHTML = `
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3900377589557710"
-            crossorigin="anonymous"></script>
-        <ins class="adsbygoogle"
-             style="display:block; text-align: center;"
-             data-ad-client="ca-pub-3900377589557710"
-             data-ad-slot="YOUR_AD_SLOT"
-             data-ad-format="auto"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3900377589557710"
+     crossorigin="anonymous"></script>
+<!-- Dystopia App 1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3900377589557710"
+     data-ad-slot="3917381126"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     `;
 
     mangaReader.appendChild(adsContainer);
